94155a98a5817b4ea4863c10e69c011f89f3708c
patch-seven <patchseven@gmail.com>
2016-05-08 22:17:58 +1000

    added swirl script

---
+++ Swirl_script.R
@@ -0,0 +1,26 @@
+install.packages("swirl")
+library(swirl)
+swirl()
+patch
+
+1
+1
+2
+swirl()
+patch
+install_from_swirl("Data_Analysis")
+1
+1
+5+7
+x <-5+7
+x
+y <- x-3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+100
+my_sqrt <- sqrt(z-1)
+2
+my_sqrt
